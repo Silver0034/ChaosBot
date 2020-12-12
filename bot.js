1f3930b7d3cdc388a8e9744944bf4e110b26b16b
Jacob <airraid.mail@gmail.com>
2020-12-11 19:44:54 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -186,54 +186,58 @@ const CONNECTION = mysql.createConnection({
   password: SECRET.MySQL.Password
 })
 
-console.log(CONNECTION)
+CONNECTION.connect(function (error) {
+  if (error) {
+    console.log(error)
+  } else {
+    // create database if it does not exist
+    CONNECTION.query('CREATE DATABASE IF NOT EXISTS ??', db, (err, results) => {
+      if (err) {
+        console.log('error in creating database', err)
+        return
+      }
 
-// create database if it does not exist
-CONNECTION.query('CREATE DATABASE IF NOT EXISTS ??', db, (err, results) => {
-  if (err) {
-    console.log('error in creating database', err)
-    return
-  }
+      // console.log('created a new database', db)
 
-  // console.log('created a new database', db)
+      // change user so I can modify database
+      CONNECTION.changeUser({
+        database: db
+      }, (err) => {
+        if (err) {
+          console.log('error in changing database', err)
+          return
+        }
 
-  // change user so I can modify database
-  CONNECTION.changeUser({
-    database: db
-  }, (err) => {
-    if (err) {
-      console.log('error in changing database', err)
-      return
-    }
+        // console.log('connected to database')
 
-    // console.log('connected to database')
+        // query string for creating a artist table
+        // var table = ('CREATE TABLE IF NOT EXISTS Artist (id INT(100) NOT NULL AUTO_INCREMENT, name TINYTEXT, PRIMARY KEY(id))')
 
-    // query string for creating a artist table
-    // var table = ('CREATE TABLE IF NOT EXISTS Artist (id INT(100) NOT NULL AUTO_INCREMENT, name TINYTEXT, PRIMARY KEY(id))')
-
-    const table = `CREATE TABLE IF NOT EXISTS guilds(
+        const table = `CREATE TABLE IF NOT EXISTS guilds(
       id VARCHAR(25) PRIMARY KEY,
       name VARCHAR(100) NOT NULL,
       apod_channel_id VARCHAR(25),
       apod_date VARCHAR(10)
     )`
 
-    CONNECTION.query(table, (err) => {
-      if (err) {
-        console.log('error in creating table', err)
-        return
-      }
+        CONNECTION.query(table, (err) => {
+          if (err) {
+            console.log('error in creating table', err)
+            return
+          }
 
-      console.log('Connected to database table')
-    })
+          console.log('Connected to database table')
+        })
 
-    // write all info from guilds table
-    CONNECTION.query('SELECT * FROM guilds', (err) => {
-      if (err) {
-        console.log('error showing table: ', err)
-      }
+        // write all info from guilds table
+        CONNECTION.query('SELECT * FROM guilds', (err) => {
+          if (err) {
+            console.log('error showing table: ', err)
+          }
+        })
+      })
     })
-  })
+  }
 })
 
 // log in to discord
