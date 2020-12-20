c64ad75db6f78dbf8e3524592ab1bb202ab63a9a
Jacob <airraid.mail@gmail.com>
2020-12-20 16:48:40 -0500

    added kanye command

---
+++ commands/kanye.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js')
+const axios = require('axios')
+
+function createEmbed (client, quote) {
+  const embed = new Discord.MessageEmbed()
+    .setTitle(':thought_balloon: Kanye Says')
+    .setColor('#43b581')
+    .setAuthor(
+      client.user.username,
+      client.user.avatarURL()
+    )
+    .setFooter(
+      `General Chaos™ | © ${new Date().getFullYear()} | Thanks to api.kanye.rest`,
+      'https://cdn.discordapp.com/icons/227552928343392256/051a39fd0c470e72a9ff52c0ddb08bf6.webp?size=256'
+    )
+    .setTimestamp()
+    .setDescription(quote.quote)
+
+  return embed
+}
+
+async function getQuote () {
+  // send a get request
+  const host = 'https://api.kanye.rest/'
+
+  const response = await axios({
+    method: 'get',
+    url: host
+  })
+
+  return response.data
+}
+
+module.exports = {
+  name: 'kanye',
+  description: ':thought_balloon: Get a quote from Kanye',
+  aliases: ['yeezus'],
+  execute (props) {
+    const { msg } = props
+    getQuote().then((quote) => {
+      if (!quote || quote.quote === false) {
+        msg.channel.send('Kanye was unavailable for comment at this time.')
+        return false
+      }
+      const embed = createEmbed(msg.client, quote)
+      msg.channel.send(embed)
+    })
+  }
+}
