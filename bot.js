c63d23cdcb512476c43e01c316cafb114eb26f07
Jacob <airraid.mail@gmail.com>
2021-01-04 13:52:51 -0500

    Commented out holidays

--- bot.js
+++ bot.js
@@ -14,146 +14,157 @@ const db = require('./functions/database')
 const reactions = require('./functions/reactions')
 const activity = require('./functions/activity')
 const apod = require('./functions/apod')
-const holidays = require('./functions/holidays')
+// const holidays = require('./functions/holidays')
 
 // * scheduled jobs
 // every 5 minutes
 schedule.scheduleJob('*/5 * * * *', () => {
-  activity.execute(client)
+	activity.execute(client)
 })
 // every morning at 8am
 schedule.scheduleJob('0 8 * * *', () => {
-  // get astronomy picture of the day
-  apod.execute({
-    client: client,
-    connection: db.connection
-  })
+	// get astronomy picture of the day
+	apod.execute({
+		client: client,
+		connection: db.connection,
+	})
 
-  holidays.execute({
-    client: client,
-    connection: db.connection
-  })
+	// holidays.execute({
+	// 	client: client,
+	// 	connection: db.connection,
+	// })
 })
 
 // get all external command files
 client.commands = new Discord.Collection()
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+const commandFiles = fs
+	.readdirSync('./commands')
+	.filter((file) => file.endsWith('.js'))
 
 // cooldowns
 const cooldowns = new Discord.Collection()
 
 // include external command files
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`)
-  client.commands.set(command.name, command)
+	const command = require(`./commands/${file}`)
+	client.commands.set(command.name, command)
 }
 
 client.on('ready', () => {
-  console.log(`${client.user.tag} is logged in and ready`)
-  // update db tables
-  db.execute(client)
+	console.log(`${client.user.tag} is logged in and ready`)
+	// update db tables
+	db.execute(client)
 
-  // set activity
-  activity.execute(client)
+	// set activity
+	activity.execute(client)
 
-  // post daily apod if after time to post
-  const startHour = new Date().getHours()
-  if (startHour > 8) {
-    // get astronomy picture of the day
-    apod.execute({
-      client: client,
-      connection: db.connection
-    })
+	// post daily apod if after time to post
+	const startHour = new Date().getHours()
+	if (startHour > 8) {
+		// get astronomy picture of the day
+		apod.execute({
+			client: client,
+			connection: db.connection,
+		})
 
-    holidays.execute({
-      client: client,
-      connection: db.connection
-    })
-  }
+		// holidays.execute({
+		//   client: client,
+		//   connection: db.connection
+		// })
+	}
 })
 
-client.on('message', msg => {
-  // add reactions to things that aren't commands
+client.on('message', (msg) => {
+	// add reactions to things that aren't commands
 
-  // don't process non-commands or messages from bots
-  if (!msg.content.startsWith(prefix) || msg.author.bot) {
-    reactions.execute(msg)
-    return
-  }
+	// don't process non-commands or messages from bots
+	if (!msg.content.startsWith(prefix) || msg.author.bot) {
+		reactions.execute(msg)
+		return
+	}
 
-  // split into arguments on space
-  const args = msg.content.slice(prefix.length).trim().split(/ +/)
-  const commandName = args.shift().toLowerCase()
+	// split into arguments on space
+	const args = msg.content.slice(prefix.length).trim().split(/ +/)
+	const commandName = args.shift().toLowerCase()
 
-  // * select the proper command
-  const command = client.commands.get(commandName) ||
-    client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
-    // quit if no command found
-  if (!command) return
+	// * select the proper command
+	const command =
+		client.commands.get(commandName) ||
+		client.commands.find(
+			(cmd) => cmd.aliases && cmd.aliases.includes(commandName)
+		)
+	// quit if no command found
+	if (!command) return
 
-  // * check if command can be run in current channel
-  if (command.guildOnly && msg.channel.type === 'dm') {
-    return msg.reply('I can\'t execute that command inside DMs!')
-  }
+	// * check if command can be run in current channel
+	if (command.guildOnly && msg.channel.type === 'dm') {
+		return msg.reply("I can't execute that command inside DMs!")
+	}
 
-  // * set cooldowns to prevent spam
-  if (!cooldowns.has(command.name)) {
-    cooldowns.set(command.name, new Discord.Collection())
-  }
-  const now = Date.now()
-  const timestamps = cooldowns.get(command.name)
-  // defaults to 3 seconds
-  const cooldownAmount = (command.cooldown || 3) * 1000
-  if (timestamps.has(msg.author.id)) {
-    const expirationTime = timestamps.get(msg.author.id) + cooldownAmount
-    if (now < expirationTime) {
-      const timeLeft = (expirationTime - now) / 1000
-      return msg.reply(`Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`)
-    }
-  }
-  timestamps.set(msg.author.id, now)
-  setTimeout(() => timestamps.delete(msg.author.id), cooldownAmount)
+	// * set cooldowns to prevent spam
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection())
+	}
+	const now = Date.now()
+	const timestamps = cooldowns.get(command.name)
+	// defaults to 3 seconds
+	const cooldownAmount = (command.cooldown || 3) * 1000
+	if (timestamps.has(msg.author.id)) {
+		const expirationTime = timestamps.get(msg.author.id) + cooldownAmount
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000
+			return msg.reply(
+				`Please wait ${timeLeft.toFixed(
+					1
+				)} more second(s) before reusing the \`${
+					command.name
+				}\` command.`
+			)
+		}
+	}
+	timestamps.set(msg.author.id, now)
+	setTimeout(() => timestamps.delete(msg.author.id), cooldownAmount)
 
-  // * check if args are required / exist
-  if (command.args && !args.length) {
-    let reply = `You did not provide the required arguments, ${msg.author}!`
-    if (command.usage) {
-      reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``
-    }
-    return msg.channel.send(reply)
-  }
+	// * check if args are required / exist
+	if (command.args && !args.length) {
+		let reply = `You did not provide the required arguments, ${msg.author}!`
+		if (command.usage) {
+			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``
+		}
+		return msg.channel.send(reply)
+	}
 
-  // * start typing
-  msg.channel.startTyping()
+	// * start typing
+	msg.channel.startTyping()
 
-  // * execute the command
-  try {
-    command.execute({
-      msg: msg,
-      args: args,
-      connection: db.connection
-    })
-  } catch (err) {
-    console.error(err)
-    msg.reply('There was an error executing that command.')
-  }
+	// * execute the command
+	try {
+		command.execute({
+			msg: msg,
+			args: args,
+			connection: db.connection,
+		})
+	} catch (err) {
+		console.error(err)
+		msg.reply('There was an error executing that command.')
+	}
 
-  // * delete the user's command
-  msg.delete().catch(() => {
-    /* do nothing because permissions issue */
-  })
+	// * delete the user's command
+	msg.delete().catch(() => {
+		/* do nothing because permissions issue */
+	})
 
-  // stop typing
-  msg.channel.stopTyping()
+	// stop typing
+	msg.channel.stopTyping()
 })
 
 client.login(token)
 
 // * send bot author a DM on error and reboot
 process.on('uncaughtException', async function (err) {
-  const user = await client.users.fetch(authorID)
+	const user = await client.users.fetch(authorID)
 
-  const message = `**Alert: There was an error**
+	const message = `**Alert: There was an error**
 
   Error:\`\`\`${err.message.replace(/`/g, '"')}\`\`\`
   
@@ -162,15 +173,15 @@ process.on('uncaughtException', async function (err) {
   I rebooted after this error
   `
 
-  const splitAt = 1500
-  const segments = message.match(new RegExp('.{1,' + splitAt + '}', 'g'))
-  const l = segments.length
-  for (let i = 0; i < l; i++) {
-    await client.users.cache.get(user.id).send(segments[i])
-  }
+	const splitAt = 1500
+	const segments = message.match(new RegExp('.{1,' + splitAt + '}', 'g'))
+	const l = segments.length
+	for (let i = 0; i < l; i++) {
+		await client.users.cache.get(user.id).send(segments[i])
+	}
 
-  console.log(err)
+	console.log(err)
 
-  // reboot everything
-  exec('sudo reboot')
+	// reboot everything
+	exec('sudo reboot')
 })
