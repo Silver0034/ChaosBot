a23d75128b55830c1446cf20958ee92495c36981
Jacob <airraid.mail@gmail.com>
2021-01-31 11:49:36 -0500

    Formatted Holiday File

--- functions/holidays.js
+++ functions/holidays.js
@@ -3,105 +3,114 @@ const { holidayKey } = require('../secret.json')
 const Discord = require('discord.js')
 const axios = require('axios')
 
-async function getHoliday () {
-  const d = new Date()
-  let month = '' + (d.getMonth() + 1)
-  let day = '' + d.getDate()
-  const year = d.getFullYear()
+async function getHoliday() {
+	const d = new Date()
+	let month = '' + (d.getMonth() + 1)
+	let day = '' + d.getDate()
+	const year = d.getFullYear()
 
-  if (month.length < 2) { month = '0' + month }
-  if (day.length < 2) { day = '0' + day }
+	if (month.length < 2) {
+		month = '0' + month
+	}
+	if (day.length < 2) {
+		day = '0' + day
+	}
 
-  // send a GET request for the image of the selected date
-  const data = {
-    api_key: holidayKey,
-    country: 'US',
-    day: day,
-    month: month,
-    year: year
-  }
-  const host = 'https://calendarific.com'
-  const path = '/api/v2/holidays'
+	// send a GET request for the image of the selected date
+	const data = {
+		api_key: holidayKey,
+		country: 'US',
+		day: day,
+		month: month,
+		year: year
+	}
+	const host = 'https://calendarific.com'
+	const path = '/api/v2/holidays'
 
-  const args = Object.keys(data)
-    .map(key => `${key}=${data[key]}`)
-    .join('&')
+	const args = Object.keys(data)
+		.map((key) => `${key}=${data[key]}`)
+		.join('&')
 
-  const response = await axios({
-    method: 'get',
-    url: `${host}${path}?${args}`
-  })
+	const response = await axios({
+		method: 'get',
+		url: `${host}${path}?${args}`
+	})
 
-  // save object
-  module.exports.holidays = response.data.response.holidays
+	// save object
+	module.exports.holidays = response.data.response.holidays
 
-  // return apod
-  return response.data.response.holidays
+	// return apod
+	return response.data.response.holidays
 }
 
-function createEmbed (client, holidays) {
-  const holiday = holidays[0]
-  const embed = new Discord.MessageEmbed()
-    .setTitle(`:calendar_spiral: ${holiday.name}`)
-    .setColor('#43b581')
-    .setAuthor(
-      client.user.username,
-      client.user.avatarURL()
-    )
-    .setFooter(
-      `General Chaos™ | © ${new Date().getFullYear()} | Thanks to calendarific.com`,
-      'https://cdn.discordapp.com/icons/227552928343392256/051a39fd0c470e72a9ff52c0ddb08bf6.webp?size=256'
-    )
-    .setTimestamp()
-    .setDescription(holiday.description)
+function createEmbed(client, holidays) {
+	const holiday = holidays[0]
+	const embed = new Discord.MessageEmbed()
+		.setTitle(`:calendar_spiral: ${holiday.name}`)
+		.setColor('#43b581')
+		.setAuthor(client.user.username, client.user.avatarURL())
+		.setFooter(
+			`General Chaos™ | © ${new Date().getFullYear()} | Thanks to calendarific.com`,
+			'https://cdn.discordapp.com/icons/227552928343392256/051a39fd0c470e72a9ff52c0ddb08bf6.webp?size=256'
+		)
+		.setTimestamp()
+		.setDescription(holiday.description)
 
-  return embed
+	return embed
 }
 
 module.exports = {
-  holiday: {},
-  execute (props) {
-    const { client, connection } = props
-    getHoliday().then((holidays) => {
-      connection.query('SELECT id, name, holiday_channel_id, holiday_date FROM guilds', (err, results) => {
-        if (err) {
-          console.log('Error with holidays from DB: ', err)
-          return
-        }
+	holiday: {},
+	execute(props) {
+		const { client, connection } = props
+		getHoliday().then((holidays) => {
+			connection.query(
+				'SELECT id, name, holiday_channel_id, holiday_date FROM guilds',
+				(err, results) => {
+					if (err) {
+						console.log('Error with holidays from DB: ', err)
+						return
+					}
 
-        // don't send if not a holiday
-        if (!holidays || holidays.length === 0) return
+					// don't send if not a holiday
+					if (!holidays || holidays.length === 0) return
 
-        const embed = createEmbed(client, holidays)
+					const embed = createEmbed(client, holidays)
 
-        results.map(result => {
-          // send a embed to each individual channel
-          if (
-            result.holiday_channel_id &&
-            result.holiday_date !== holidays[0].date.iso
-          ) {
-            // hasn't already set a embed today
-            client.channels.cache.get(result.holiday_channel_id).send(embed)
+					results.map((result) => {
+						// send a embed to each individual channel
+						if (
+							result.holiday_channel_id &&
+							result.holiday_date !== holidays[0].date.iso
+						) {
+							// hasn't already set a embed today
+							client.channels.cache
+								.get(result.holiday_channel_id)
+								.send(embed)
 
-            // add to db that it was sent
-            let query = `INSERT INTO guilds (id, name, holiday_date)
+							// add to db that it was sent
+							let query = `INSERT INTO guilds (id, name, holiday_date)
                     VALUES
                   `
-            query += `("${result.id}", "${result.name}", "${holidays[0].date.iso}")`
-            query += `
+							query += `("${result.id}", "${result.name}", "${holidays[0].date.iso}")`
+							query += `
                   ON DUPLICATE KEY UPDATE
                   name = VALUES(name),
                   holiday_date = VALUES(holiday_date)`
 
-            connection.query(query, (err) => {
-              if (err) {
-                console.log('Error saving holiday_date: ', err)
-              }
-            })
-          }
-          return true
-        })
-      })
-    })
-  }
+							connection.query(query, (err) => {
+								if (err) {
+									console.log(
+										'Error saving holiday_date: ',
+										err
+									)
+								}
+							})
+						}
+						return true
+					})
+				}
+			)
+		})
+	}
 }
